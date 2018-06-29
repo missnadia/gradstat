d6091dc1e4db52cefca6fcfd81287f6435b34e3c
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-29 19:31:54 +0000

    add sessions methods to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :require_login, except: [:home, :new, :create]
+  helper_method :current_user
+
+  private
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    @current_user ||= Student.find_by(id: session[:student_id]) if session[:student_id]
+  end
+
+  def require_login
+    redirect_to root_path unless logged_in?
+  end
 end

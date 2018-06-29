a3f46a1453ee0859e34649ddeb9f05802db6826a
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-29 19:05:35 +0000

    add routes to StudentsController

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,47 @@
 class StudentsController < ApplicationRecord
+  before_action :set_student, only: [:show, :edit, :update, :destroy]
 
+  def show
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def edit
+  end
+
+  def create
+    @student = Student.create(student_params)
+    if @student.save
+      session[:student_id] = @student.id
+      redirect_to student_path(@student)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @student.update(student_params)
+      redirect_to student_path(@student)
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(
+      :username,
+      :password,
+      :email,
+      :first_name,
+      :last_name
+    )
+  end
 end

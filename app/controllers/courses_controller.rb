df10bf7093164698340f277675b5f7e61b9f42f7
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-29 19:37:27 +0000

    add code to CoursesController

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,3 +1,44 @@
 class CoursesController < ApplicationController
+  before_action :set_course, only: [:show, :edit, :update]
 
+  def index
+    @course = Course.all
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def edit
+  end
+
+  def create
+    @course = Course.create(course_params)
+    if @course.save
+      redirect_to course_path(@course)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @course.update(course_params)
+      redirect_to course_path(@course)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(
+      :name,
+      :hours_spent,
+      :completed
+    )
 end

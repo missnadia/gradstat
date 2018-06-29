610622835b39875b03d70002fca7286e032721ea
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-29 19:16:17 +0000

    add SessionsController routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'static#home'
+  
   resources :students
   resources :courses
   resources :goals
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'session#create'
+  delete '/signout' => 'sessions#destroy'
 end

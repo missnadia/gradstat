29f1c503265eebf8d7a9a914f94d7342877f321e
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-29 19:42:58 +0000

    add code to GoalsController

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,17 @@
 class GoalsController < ApplicationRecord
+  def new
+    @goal = Goal.create(goal_params)
+    redirect_to student_path(@goal.student)
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(
+      :student_id,
+      :course_id,
+      :attained,
+      :date
+    )
+  end
 end

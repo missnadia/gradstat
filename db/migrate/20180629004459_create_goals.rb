8aca5f7c9a31edb3c0b752e4b7eec1ad33ff0975
Nadia Lee <32117376+missnadia@users.noreply.github.com>
2018-07-06 16:34:14 -0400

    update goals migration column

--- db/migrate/20180629004459_create_goals.rb
+++ db/migrate/20180629004459_create_goals.rb
@@ -3,7 +3,7 @@ class CreateGoals < ActiveRecord::Migration[5.2]
     create_table :goals do |t|
       t.belongs_to :student, foreign_key: true
       t.belongs_to :course, foreign_key: true
-      t.boolean :attained
+      t.boolean :completed
       t.datetime :date
 
       t.timestamps null: false

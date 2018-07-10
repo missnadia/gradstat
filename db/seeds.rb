b7a66eae25065be58e2a71a4b9a86358971080ae
missnadia <miss.nadia.lee@gmail.com>
2018-07-10 10:55:49 -0700

    add seed data to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+students = Student.create([{ 
+    username: "sfields",
+    password: "password",
+    email: "sallyfields@abc.com",
+    first_name: "Sally",
+    last_name: "Fields",
+    admin: true
+}])

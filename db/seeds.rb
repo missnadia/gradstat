adc820349de375dcc60f36e8392867d55002c701
missnadia <miss.nadia.lee@gmail.com>
2018-07-10 19:22:25 +0000

    add data to db/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,4 +13,31 @@ students = Student.create([{
     first_name: "Sally",
     last_name: "Fields",
     admin: true
+},
+{
+    username: "tbear",
+    password: "password",
+    email: "tbear@efg.com",
+    first_name: "Teddy",
+    last_name: "Bear",
+    admin: false
 }])
+
+courses = Course.create([
+    { name: "Introduction to Rails" },
+    { name: "Rails Application Basics" },
+    { name: "Rails MVC" },
+    { name: "Rails Static Request" },
+    { name: "Rails Hello World" },
+    { name: "Video Review: Intro to Rails" },
+    { name: "ActiveRecord Models and Rails" },
+    { name: "ActiveRecord Models and Rails Lab" },
+    { name: "Intro to REST" },
+    { name: "RESTful Action Index Lab" },
+    { name: "Rails Dynamic Request" },
+    { name: "Rails Dynamic Request Lab" },
+    { name: "Rails URL Helpers" },
+    { name: "Rails URL Helpers Lab" },
+    { name: "Rails form_tag" },
+    { name: "Rails form_tag Lab" }
+])

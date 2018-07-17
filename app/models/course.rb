d61578ba2b96b2dd1a84fbba0cb1df49902b7606
missnadia <miss.nadia.lee@gmail.com>
2018-07-17 01:39:40 +0000

    add class methods to course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,24 +1,18 @@
 class Course < ApplicationRecord
   has_many :charts
   has_many :students, through: :charts
+  validates :name, presence: true, uniqueness: true
+  validates :date, presence: true
 
-  LESSONS = [
-    "Introduction to Rails",
-    "Rails Application Basics",
-    "Rails MVC",
-    "Rails Static Request",
-    "Rails Hello World",
-    "Video Review: Intro to Rails",
-    "ActiveRecord Models and Rails",
-    "ActiveRecord Models and Rails Lab",
-    "Intro to REST",
-    "RESTful Action Index Lab",
-    "Rails Dynamic Request",
-    "Rails Dynamic Request Lab",
-    "Rails URL Helpers",
-    "Rails URL Helpers Lab",
-    "Rails form_tag",
-    "Rails form_tag Lab"
-  ]
+  def self.total_hours
+    self.sum(:time_spent)
+  end
 
+  def self.total_completed
+    self.where(completed: true).count
+  end
+
+  def self.total_lessons
+    self.distinct.count(:name)
+  end
 end

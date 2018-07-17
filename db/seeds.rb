53ba41ae2096e1b16102937bd16b8289132226fb
missnadia <miss.nadia.lee@gmail.com>
2018-07-17 01:38:48 +0000

    add course names to db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,22 @@ students = Student.create([{
     last_name: "Bear",
     admin: false
 }])
+
+courses = Course.create([
+    { name: "Introduction to Rails" },
+    { name: "Rails Application Basics"},
+    { name: "Rails MVC"},
+    { name: "Rails Static Request"},
+    { name: "Rails Hello World"},
+    { name: "Video Review: Intro to Rails"},
+    { name: "ActiveRecord Models and Rails"},
+    { name: "ActiveRecord Models and Rails Lab"},
+    { name: "Intro to REST"},
+    { name: "RESTful Action Index Lab"},
+    { name: "Rails Dynamic Request"},
+    { name: "Rails Dynamic Request Lab"},
+    { name: "Rails URL Helpers"},
+    { name: "Rails URL Helpers Lab"},
+    { name: "Rails form_tag"},
+    { name: "Rails form_tag Lab" }
+])

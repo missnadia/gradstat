9273b6bcb966fb6b6d4ee4fa483e21ec5b8cbf19
missnadia <miss.nadia.lee@gmail.com>
2018-08-10 05:31:16 +0000

    refactor validations and add date error method

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,24 +1,30 @@
 class Student < ApplicationRecord
+  devise :omniauthable, :omniauth_providers => [:facebook]
   has_many :course_students
   has_many :courses, through: :course_students
   has_many :courses
   has_secure_password
-  validates :username, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates_presence_of :username, :first_name, :last_name
 
   def full_name
     self.first_name + " " + self.last_name
   end
-
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |student|
-        student.provider = auth.provider
-        student.uid = auth.uid
-        student.name = auth.info.name
-        student.oauth_token = auth.credentials.token
-        student.oauth_expires_at = Time.at(auth.credentials.expires_at)
-        student.save!
+  
+  def self.new_with_session(params, session)
+    super.tap do |student|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        student.email = data["email"] if student.email.blank?
+      end
     end
-end
+  end
+  
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |student|
+      student.email = auth.info.email
+      student.password = Devise.friendly_token[0,20]
+      student.first_name = auth.info.first_name
+      student.last_name = auth.info.last_name
+    end
+  end
 end
